1eb6d9dd1f9a61a01014cffbb23fa6a3ec47e8e0
agustinparmisano <agustinparmisano@gmail.com>
2017-04-03 18:56:04 -0300

    Agregando punto 2 inverso

---
+++ pract1/2_inverso.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+list=($(ls))
+size=${#list[*]}
+size=$(expr $size - 1)
+newlist=()
+i=0
+
+echo "Lista de archivos del directorio actual: ${list[*]}"
+echo "Tamaño de lista de archivos: $size"
+
+for f in "${list[@]}"; do
+	newlist[i]=${list[$size]}
+	#echo "Fin de lista: ${list[$size]}"
+	i=$(expr $i + 1)
+	#echo "i es $i"
+	#echo "size es $size"
+	size=$(expr $size - 1)
+done
+
+echo "Lista de archivos inversa ${newlist[*]}"
